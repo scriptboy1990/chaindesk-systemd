4182556eddea22ef78b2a85c2c6282214d7269f3
MorgenDigital <95627378+morgendigital@users.noreply.github.com>
2023-08-20 23:34:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 # Reload systemd and start the services
 sudo systemctl daemon-reload
-sudo systemctl start load-sissi-data
-sudo systemctl start start-sissi-app
+sudo systemctl start chaindesk-load-data
+sudo systemctl start chaindesk-start-app
 # Enable the services to start on boot
-sudo systemctl enable load-sissi-data
-sudo systemctl enable start-sissi-app
+sudo systemctl enable chaindesk-load-data
+sudo systemctl enable chaindesk-start-app
 # To view logs: journalctl -u start-sissi-app
 # To monitor service: systemctl status start-sissi-app
 exit 0
