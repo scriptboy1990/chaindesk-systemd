f89d03ba24eb5f402ffd918c728dad32094258ac
MorgenDigital <95627378+morgendigital@users.noreply.github.com>
2023-08-20 23:49:15 +0200

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+# Todo: This executable will deploy Chaindesk with custom settings
+# 1. Grab latest Chaindesk from Github
+# 2. Change URLs to your own in one shot
+# 3. Install prequisites like NPM and PNPM
+# 4. Build and run (dev or prod)
